1c82c2bbd25098025d4a762e08741de7b5f453b3
David Herron <david@davidherron.com>
2020-04-04 16:12:03 -0700

    Fix code-embed example

--- config.js
+++ config.js
@@ -212,14 +212,14 @@ config
     .addFooterJavaScript({ href: "/vendor/bootstrap/js/bootstrap.min.js" })
     .addFooterJavaScript({ href: "/vendor/highlight.js/lib/highlight.js" })
     // .addFooterJavaScript({ script: 'hljs.initHighlightingOnLoad();' })
-    .addFooterJavaScript({
-        script: `
-        document.addEventListener('DOMContentLoaded', (event) => {
-            document.querySelectorAll('pre code').forEach((block) => {
-              hljs.highlightBlock(block);
-            });
-        });
-        `})
+    // .addFooterJavaScript({
+    //    script: `
+    //    document.addEventListener('DOMContentLoaded', (event) => {
+    //        document.querySelectorAll('pre code').forEach((block) => {
+    //          hljs.highlightBlock(block);
+    //        });
+    //    });
+    //    `})
     .addStylesheet({ href: "/vendor/bootstrap/css/bootstrap.min.css" })
     /* .addStylesheet({
         href: "/vendor/bootstrap/css/bootstrap-theme.min.css"
